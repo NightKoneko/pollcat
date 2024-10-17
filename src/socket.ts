c72a834d5c0073619a12ef0fd3e19a763f7410d4
NightKoneko <catzruleiscool@gmail.com>
2024-10-17 15:07:13 -0400

    .

--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // Make sure this URL is your Render backend URL
-const backendURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
+const backendURL = process.env.REACT_APP_BACKEND_URL
 const socket = io(backendURL, { transports: ['websocket'] }); 
 
 export default socket;
