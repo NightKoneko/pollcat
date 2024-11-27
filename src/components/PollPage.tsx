894a847044c2cd879b93cbfeed2703696c620521
NightKoneko <catzruleiscool@gmail.com>
2024-11-27 14:04:39 -0500

    fix imports on PollPage.tsx

--- src/components/PollPage.tsx
+++ src/components/PollPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import socket from '../socket.ts';
+//import socket from '../socket.ts';
 import PollVoting from './PollVoting';
 
 interface Poll {
